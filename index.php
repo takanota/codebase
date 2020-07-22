a699bbabf654cf2b75dfb5f4c866e00271ea11de
Takahiro Takano <3506204+takanota@users.noreply.github.com>
2020-07-22 12:48:59 +0900

    Add beforeunload eventhandler

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 <html>
 <head>
   <title>hello</title>
+  <script type="text/javascript">
+    window.beforeunload = function (e) {
+      e.returnValue = "are you sure?";
+    };
+  </script>
 </head>
 <body>
   <h1>Hello</h1>
